fa08c470da1964fe91e3c62db1cfcf9be6d6d450
Antonio <adelarosariquelme1999@gmail.com>
2025-03-25 11:35:20 +0100

    Actualizar título de la página de inicio de sesión a "Panel Administrador"

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-4 text-center text-black" >Iniciar Sesión</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center text-black" >Panel Administrador</h1>
         
         {error && <p className="text-red-500 text-center">{error}</p>}
         
