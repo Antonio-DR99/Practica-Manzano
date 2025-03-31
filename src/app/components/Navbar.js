413716582317cf2f64e79b4845f0974ae9885457
Alex Martinez <genexix05@gmail.com>
2025-03-31 11:46:18 +0200

    feat: traducir etiqueta "Orders" a "Pedidos" en el Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -90,7 +90,7 @@ export default function Navbar() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
             </svg>
-            <span className="text-gray-700">Orders</span>
+            <span className="text-gray-700">Pedidos</span>
           </Link>
           
           <Link href="/pages/productos" 
