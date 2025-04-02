535ed4168b4ea4ef3197e8f9b7cf4457f118e474
Alex Martinez <genexix05@gmail.com>
2025-04-02 10:16:55 +0200

    fix: cambiar el texto del enlace de "Citas" a "Log-in" en el Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -230,7 +230,7 @@ export default function Navbar() {
                   d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                 />
               </svg>
-              <span className="text-gray-700">Citas</span>
+              <span className="text-gray-700">Log-in</span>
             </Link>
           </div>
         </div>
